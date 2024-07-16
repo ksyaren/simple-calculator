a99260b4e28a64b5877ecd5c163adfd2afbd9c39
Yaren Kose <ksyarenn@gmail.com>
2024-07-16 16:39:23 +0300

    operation screen added

--- calculator.py
+++ calculator.py
@@ -7,8 +7,8 @@ window.geometry("470x500+100+200")
 window.resizable(False,False)
 window.configure(background="#17161b")
 
-
-
+label_result = Label(window,width=25, height=2, text="", font=("Arial", 30))
+label_result.pack()
 
 
 
