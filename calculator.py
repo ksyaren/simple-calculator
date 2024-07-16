bea628b5e2378acce4279e1f7db4e86697850582
Yaren Kose <ksyarenn@gmail.com>
2024-07-16 16:36:10 +0300

    project window created

---
+++ calculator.py
@@ -0,0 +1,15 @@
+import tkinter
+from tkinter import *
+
+window = Tk()
+window.title("Calculator")
+window.geometry("470x500+100+200")
+window.resizable(False,False)
+window.configure(background="#17161b")
+
+
+
+
+
+
+window.mainloop()
